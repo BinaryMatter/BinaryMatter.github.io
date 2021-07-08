9bb6b78592267a50ea479235efe5b7ea076a29de
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-07-08 16:39:46 +0530

    Email Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Binary Matter</h1>
+      <h3>Contact Us : </h3>
+      <a href="mailto:infobinarymatter@gmail.com">infobinarymatter@gmail.com</a>
+
     </div>
   );
 }
